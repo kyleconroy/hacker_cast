772b2391c64f87f5a22ed271cfd1e6df28bfb74b
John Terry <jpterry@gmail.com>
2013-02-01 19:59:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerCast::Application.config.secret_token = 'b6c5313871084b808572980eadd3aaba1d2b267c7141c77f97d0736d763dc811c180506abb7cd3bed70c7613ad5d0d46b7b8f3db199bcaf5ab5158f632a8af99'
